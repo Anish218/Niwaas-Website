201302d54eb506d884491b5432494db018c6c687
Anish218 <anishmishram218@gmail.com>
2022-10-07 00:18:46 +0530

    did some modification with optimisation

---
+++ main/java/com/knf/dev/request/ForgotPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.knf.dev.request;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class ForgotPasswordRequest {
+    private String username;
+    private String newpassword;
+    private String confirmnewpassword;
+}
